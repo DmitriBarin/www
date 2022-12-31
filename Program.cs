7b034001ab0c92e7c0c352b22fd681aa44039f82
DmitriyBarinov <dmitriy.barinow@yandex.ru>
2022-12-31 13:02:09 +0300

    c игнором

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Код с лекции: (не работает username, не присваевает)
+//Console.WriteLine("Ведите Ваше имя");
+//String username = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine("username");
+
+//подсказали команду. Работает!
+Console.WriteLine("Ведите Ваше имя");
+String username = Convert.ToString (Console.ReadLine());
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+//Console.WriteLine($”Привет, {username}”); обьеденить две последние строки
+
+//Console.WriteLine("Ведите Ваше имя: ");
+//String username = Convert.ToString (Console.ReadLine());
+//Console.Write("Привет, ") ;
+//Console.Write(username);
+
+//Console.WriteLine("Ведите Ваше имя");
+//String username = Convert.ToString (Console.ReadLine());
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(username);
